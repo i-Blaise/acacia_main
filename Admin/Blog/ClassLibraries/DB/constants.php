a7e2006930524f5a8311c9d758dd4caccb883c2e
i-Blaise <menniablaise@yahoo.com>
2023-04-14 23:53:14 +0000

    aborts

--- Admin/Blog/ClassLibraries/DB/constants.php
+++ Admin/Blog/ClassLibraries/DB/constants.php
@@ -5,9 +5,14 @@ session_start();
 // define('DB_PASS' ,'');
 // define('DB_NAME', 'acacia_blog');
 
-define('DB_HOST','ahighana.com');
-define('DB_PORT','3306');
+// define('DB_HOST','ahighana.com');
+// define('DB_PORT','3306');
+// define('DB_SERVER','localhost');
+// define('DB_USER','acaciah1_hquizuser');
+// define('DB_PASS' ,'dNVnY?mp5?9CZd!9');
+// define('DB_NAME', 'acaciah1_hquiz');
+
 define('DB_SERVER','localhost');
-define('DB_USER','acaciah1_hquizuser');
-define('DB_PASS' ,'dNVnY?mp5?9CZd!9');
-define('DB_NAME', 'acaciah1_hquiz');
+define('DB_USER','mcgnuser');
+define('DB_PASS' ,'mcgnroot');
+define('DB_NAME', 'acacia');
