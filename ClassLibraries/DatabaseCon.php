4343f6b7759877139fc2846263a1f6889bb56a6d
i-Blaise <menniablaise@yahoo.com>
2023-04-15 00:11:10 +0000

    display errors

--- ClassLibraries/DatabaseCon.php
+++ ClassLibraries/DatabaseCon.php
@@ -4,6 +4,10 @@ session_start();
 // define('DB_USER','root');
 // define('DB_PASS' ,'');
 // define('DB_NAME', 'acacia.quizz');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 
 define('DB_SERVER','localhost');
 define('DB_USER','mcgnuser');
